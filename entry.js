fa9689fc2c939276d00c7dd4ac150526e35a7c14
Alexis Bellet <info@alexis-bellet.com>
2017-01-27 08:24:01 -0500

    init: upload project boilerplate ready for React using webpack

---
+++ entry.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require("file?name=[name].[ext]!./index.html");
+require("!style-loader!css-loader!sass-loader!./styles.scss");
+
+// ReactDOM.render(<TodoApp />, document.getElementById("view-container"));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: "dist",
+        filename: "bundle.js"
+    },
+    module: {
+      loaders: [
+        {
+          test: /.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['react', 'es2015']
+          }
+        },
+        {
+          test: /\.scss$/,
+          loaders: ["style-loader", "css-loader", "sass-loader"]
+        }
+      ]
+    }
+};
