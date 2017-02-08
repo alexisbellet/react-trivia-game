e23d83104e4e9b6adcdd0722011477efe8846264
Lisa <lisafolkerson@gmail.com>
2017-02-07 21:27:06 -0500

    feat: move database to firebase. App Layout now stateless component

--- layout/app_layout.js
+++ layout/app_layout.js
@@ -7,44 +7,6 @@ import Header from '../components/header';
 class AppLayout extends React.Component {
 	constructor() {
 		super();
-		this.state = {
-			quizes: {
-				quiz1: {
-					name: 'JavaScript Facts and Fun',
-					category: 'JavaScript',
-					questions: {
-						question1: 'Who is the inventor of JSON?',
-						answers1: {
-							correct: 'Douglas Crockford',
-							incorrect: ['Brendan Eich', 'Linus Torvalds', 'Grace Hopper'],
-							source1: ''
-						},
-						question2: 'Which of the following is NOT a JavaScript framework?',
-						answers2: {
-							correct: 'Elm',
-							incorrect: ['Dojo', 'Ractive', 'Marionette'],
-							source: ''
-						}						
-					}
-				},
-				quiz2: {
-					name: 'Popular Algorithms',
-					category: 'Computer Science',
-					questions: {
-						question1: 'Which culture, have we found, was the first to use Algorithms?',
-						answers1: {
-							correct: 'Babylonians in 1600s BCE',
-							incorrect: ['Egyptians in 1300s BCE', 'Sumerians in 3100s BCE ', 'Persians in 800s BCE'],
-							source: 'https://medium.com/@_marcos_otero/the-real-10-algorithms-that-dominate-our-world-e95fa9f16c04#.p90x20j1x'					
-						}
-					}
-				}
-			},
-			user: {
-				name: '',
-				avatar: ''
-			}
-		};
 	}
 
 	// we could potentially add a footer component if we want
