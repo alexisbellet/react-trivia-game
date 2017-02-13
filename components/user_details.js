6d42c542d32df1f4930cd19352a02cbfd71f533e
Lisa <lisafolkerson@gmail.com>
2017-02-12 19:12:19 -0500

    feat: login but no signup yet

--- components/user_details.js
+++ components/user_details.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import firebase from 'firebase';
+import Login from './login';
+
 
 class UserDetails extends React.Component {
+	constructor() {
+	  super()
+	  this.state = {
+	    currentUser: null,
+	    loggedIn: false
+	  }
+	  // this.login = this.login.bind(this);
+	}
+
 	render() {
 		return (
 		  	<div className="userDetails">
 		  		<img className="userDetails--avatar" src="../assets/min-code-on-screen.jpg" alt="User Avatar"/>
-		  		<p className="userName">lisa</p>
+
+		  		{
+		  			!this.state.loggedIn ? <Login onLogin={ (userName) => this.login(userName) }/> :  this.state.currentUser 
+		  		}
+
 		  	</div>
 		)
+	}
 
+	login(userName) {
+	  this.setState({ loggedIn: true, currentUser: userName });
 	}
 }
 
