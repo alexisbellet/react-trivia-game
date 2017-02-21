20cefad091a598c27e0955fec7ad45f6f883b2ce
Lisa <lisafolkerson@gmail.com>
2017-02-21 17:16:37 -0500

    chore: add user score prog bar

--- components/top_scores.js
+++ components/top_scores.js
@@ -13,24 +13,36 @@ class TopScores extends React.Component {
 
 	render() {
 		return (
-			<div className="top-scores col-xs-3 col-sm-3 col-md-3 col-lg-3">
+			<div className="top-scores">
 				<p className="highScore--list  highScore--list__currentUser">You:</p>
-				<div>
+				<div className="highScore--User  highScore--User__thisUser">
 					<img src="" alt="User Avatar" className="userAvatar"/>
 					<p className="display">{this.props.user}</p>
+					<div className="scoreProgress">
+						<div className="scoreProgress--filled"></div>
+					</div>
 				</div>
 				<p className="highScore--list">High Scores:</p>
-				<div>
+				<div className="highScore--User">
 					<img src="" alt="User Avatar" className="userAvatar"/>
-					<p className="display">No. 1 High Score</p>
+					<p className="display">No. 1 High Score Username</p>
+					<div className="scoreProgress">
+						<div className="scoreProgress--filled"></div>
+					</div>
 				</div>
-				<div>
+				<div className="highScore--User">
 					<img src="" alt="User Avatar" className="userAvatar"/>
-					<p className="display">No. 2 High Score</p>
+					<p className="display">No. 2 High Score Username</p>
+					<div className="scoreProgress">
+						<div className="scoreProgress--filled"></div>
+					</div>
 				</div>
-				<div>
+				<div className="highScore--User">
 					<img src="" alt="User Avatar" className="userAvatar"/>
-					<p className="display">No. 3 High Score</p>
+					<p className="display">No. 3 High Score Username</p>
+					<div className="scoreProgress">
+						<div className="scoreProgress--filled"></div>
+					</div>
 				</div>
 			</div>
 		)
