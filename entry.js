9cbe4d606f801fdedd8981e96492a5c14f6f78fa
Alexis Bellet <info@alexis-bellet.com>
2017-02-26 11:38:40 -0500

    feat: add pageNotFound

--- entry.js
+++ entry.js
@@ -8,6 +8,7 @@ import HomePage from './views/home_page';
 import TopicPage from './views/topic_page';
 import LogInPage from './views/login_page';
 import SignUpPage from './views/signup_page';
+import PageNotFound from './views/notfound_page';
 
 require("file?name=[name].[ext]!./index.html");
 require("./styles.scss");
@@ -34,7 +35,7 @@ ReactDOM.render((
 			<Route path="topic/:topic" component={TopicPage}/>
 			<Route path="sign-up" component={SignUpPage} />
 			<Route path="log-in" component={LogInPage} />
-			<Route path="*" component={HomePage}/>
+			<Route path="*" component={PageNotFound}/>
 		</Route>
 	</Router>
 ), document.getElementById("view-container"));
