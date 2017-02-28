27974740012777d62efa4ddc2cd1aa15ad435866
Alexis Bellet <info@alexis-bellet.com>
2017-02-28 10:32:54 -0500

    chore: refactor some code, feat: add "ready for quiz?" step before quiz start

--- components/quiz.js
+++ components/quiz.js
@@ -7,23 +7,25 @@ class Quiz extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			hasQuizStarted: false,
 			questionsWithShuffledAnswers: [],
       currentQuestion: 0,
       initialCountdownTime: 20,
-      timePerQuestion: {},
       remainingTime: 20,
       hasUserAnswered: false,
-      displayCorrectnessCorrect: false,
-      totalScoreForQuiz: 0,
+      hasAnsweredCorrectly: false,
       correctAnswer: null,
-      pointsThisQuestion: 0
+      pointsThisQuestion: 0,
+      timePerQuestion: {},
+      totalScoreForQuiz: 0
     }
-    this.checkCorrectness = this.checkCorrectness.bind(this);
     this.shuffleAnswers = this.shuffleAnswers.bind(this);
+    this.startQuiz = this.startQuiz.bind(this);
     this.updateTimer = this.updateTimer.bind(this);
-    this.advanceQuiz = this.advanceQuiz.bind(this);
+    this.checkCorrectness = this.checkCorrectness.bind(this);
     this.calculateScore = this.calculateScore.bind(this);
     // this.updateSharedScoreInfo = this.updateSharedScoreInfo.bind(this);
+    this.advanceQuiz = this.advanceQuiz.bind(this);
 	}
 
 	componentWillReceiveProps(nextProps) {
@@ -32,23 +34,36 @@ class Quiz extends React.Component {
 		}
 	}
 
+	prepareForQuiz() {
+		return (
+			<div className="prepare-for-quiz">
+				<h3>Ready to start the quiz?</h3>
+				<button className="btn" onClick={ () => this.startQuiz() }>Click here!</button>
+			</div>
+		)
+	}
+
 	displayQuiz() {
 		const { questionsWithShuffledAnswers, 
 						currentQuestion, 
 						remainingTime, 
 						hasUserAnswered, 
 						timePerQuestion,
-						pointsThisQuestion } = this.state;
+						pointsThisQuestion,
+						hasAnsweredCorrectly,
+						totalScoreForQuiz } = this.state;
 
 		if (questionsWithShuffledAnswers.length < 1) {
 			return (<div>Quiz is loading...</div>)
+
 		} else if ( questionsWithShuffledAnswers.length < (currentQuestion + 1) ){
 			return(
 					<div className="">
-						<h3>Your final score is { this.state.totalScoreForQuiz }</h3>
+						<h3>Your final score is { totalScoreForQuiz }</h3>
 						<Link to="/">Return to all Quizzes</Link>
 					</div>
 				)
+
 		} else {
 			return (questionsWithShuffledAnswers.map((question, i) => {
 				
@@ -68,21 +83,15 @@ class Quiz extends React.Component {
 							</button>
 						) ) }
 
-						{ hasUserAnswered === true && this.state.displayCorrectnessCorrect === true ? 
+						{ hasUserAnswered === true ?
 								<div>
-									<p>you answered the question correctly, you get {pointsThisQuestion} points</p> 
-									<p>your score is { this.state.totalScoreForQuiz }</p> 
+									{ hasAnsweredCorrectly === true ? 
+										<p>You answered the question correctly, you get {pointsThisQuestion} points</p> :
+										<p>You answered the question incorrectly. You get 0 new points...</p> }
+									<p>Your score is { totalScoreForQuiz }</p> 
 									<button onClick={(evt) => this.advanceQuiz(evt.target.value, currentQuestion)}>Next Question</button>
 								</div>
 							: null }
-						{ hasUserAnswered === true && this.state.displayCorrectnessCorrect === false ? 
-								<div>
-									<p>you answered the question incorrectly. You get 0 new points...</p>
-									<p>the correct answer is {this.state.correctAnswer}</p> 
-									<p>your score is { this.state.totalScoreForQuiz }</p> 
-									<button onClick={(evt) => this.advanceQuiz(evt.target.value, currentQuestion)}>Next Question</button>
-								</div>
-							: null }	
 					</div>
 				)}
 			))
@@ -90,20 +99,19 @@ class Quiz extends React.Component {
 	}
 
 	render() {
-		const { questionsWithShuffledAnswers, 
-						currentQuestion, 
-						remainingTime, 
+		const { hasQuizStarted,
 						hasUserAnswered, 
-						timePerQuestion, 
-						totalScoreForQuiz,
-						pointsThisQuestion } = this.state;
+						currentQuestion,
+						questionsWithShuffledAnswers, 
+						remainingTime, 
+						timePerQuestion } = this.state;
 
-		// displaying container for "content"
+		// before the quiz starts, display a welcome screen and ask the user for action to start the quiz
 		return (
 			<main className="quiz-container col-xs-9 col-sm-9 col-md-9 col-lg-9" >
-				{ this.displayQuiz() }
+				{ hasQuizStarted ? this.displayQuiz() : this.prepareForQuiz() }
 
-				{ hasUserAnswered ?
+				{ hasUserAnswered || !hasQuizStarted ?
 					(<div className="hide"></div>) :
 					(<Countdown currentQuestion={ currentQuestion }
 									 		amountOfQuestions= { questionsWithShuffledAnswers.length }
@@ -113,41 +121,6 @@ class Quiz extends React.Component {
 									 		timePerQuestion={ timePerQuestion }/>) }
 			</main>
 		)
-	}
-
-	calculateScore(allPoints){
-		allPoints = Object.values(allPoints);
-		allPoints = allPoints.map(Number);
-
-		let streakArray = [];
-
-		allPoints = allPoints.map((num, i) => {
-			if (num === 0) {
-				streakArray = [];
-			} else {
-
-				if (streakArray.length == 0 ) { // there is no streak so stays
-					streakArray.push(num);
-					// console.log('no streak length is' + streakArray.length + ' so num is ' + num, );
-
-				} else if (streakArray.length !== 0){ // streak so multiply
-					streakArray.push(num);
-					let newMultiplier = ((streakArray.length * 2) / 10) + 1;
-					num = (num * newMultiplier);
-					// console.log('mult ' + newMultiplier);
-				}
-			}
-			return Math.round(num);
-		});
-
-		const result = allPoints.reduce((a, b) => a + b, 0);
-		const pointsThisQuestion = allPoints[allPoints.length-1];
-		// console.log(allPoints + ' add up to the all points ' + result);
-
-		this.setState({
-			totalScoreForQuiz: result,
-			pointsThisQuestion: pointsThisQuestion
-		}) ;
 	}
 
 	shuffleAnswers(questions) {
@@ -202,26 +175,10 @@ class Quiz extends React.Component {
 		}
 	}
 
-	updateTimer(newTime) {
+	startQuiz() {
 		this.setState({
-			remainingTime: newTime
+			hasQuizStarted: true
 		});
-	}
-
-	displayCorrectAnswer(correctAnswer){
-		this.setState({
-			hasUserAnswered: true,
-			correctAnswer: correctAnswer
-		});
-	}
-
-	advanceQuiz(index, currentQuestion){
-		// move advancing quiz by updating state here so it can be called when appropriate
-		this.setState({
-			currentQuestion: (currentQuestion + 1),
-			remainingTime: this.state.initialCountdownTime,
-			hasUserAnswered: false
-		})
 	}
 
 	checkCorrectness(answer, index) {
@@ -239,7 +196,7 @@ class Quiz extends React.Component {
 			timePerQuestion[index] = remainingTime;
 			this.setState({
 				timePerQuestion: timePerQuestion,		
-				displayCorrectnessCorrect: true
+				hasAnsweredCorrectly: true
 			});
 			this.calculateScore(timePerQuestion);
 
@@ -254,9 +211,65 @@ class Quiz extends React.Component {
 		}
 	}
 
-	componentDidUpdate(){
+	updateTimer(newTime) {
+		this.setState({
+			remainingTime: newTime
+		});
+	}
 
-		this.updateSharedScoreInfo(this.state.totalScoreForQuiz);
+	// componentDidUpdate(){
+	// 	this.updateSharedScoreInfo(this.state.totalScoreForQuiz);
+	// }
+
+	calculateScore(allPoints){
+		allPoints = Object.values(allPoints);
+		allPoints = allPoints.map(Number);
+
+		let streakArray = [];
+
+		allPoints = allPoints.map((num, i) => {
+			if (num === 0) {
+				streakArray = [];
+			} else {
+
+				if (streakArray.length == 0 ) { // there is no streak so stays
+					streakArray.push(num);
+					// console.log('no streak length is' + streakArray.length + ' so num is ' + num, );
+
+				} else if (streakArray.length !== 0){ // streak so multiply
+					streakArray.push(num);
+					let newMultiplier = ((streakArray.length * 2) / 10) + 1;
+					num = (num * newMultiplier);
+					// console.log('mult ' + newMultiplier);
+				}
+			}
+			return Math.round(num);
+		});
+
+		const result = allPoints.reduce((a, b) => a + b, 0);
+		const pointsThisQuestion = allPoints[allPoints.length-1];
+		// console.log(allPoints + ' add up to the all points ' + result);
+
+		this.setState({
+			totalScoreForQuiz: result,
+			pointsThisQuestion: pointsThisQuestion
+		}) ;
+	}
+
+	displayCorrectAnswer(correctAnswer){
+		this.setState({
+			hasUserAnswered: true,
+			correctAnswer: correctAnswer
+		});
+	}
+
+	advanceQuiz(index, currentQuestion){
+		// move advancing quiz by updating state here so it can be called when appropriate
+		this.setState({
+			currentQuestion: (currentQuestion + 1),
+			remainingTime: this.state.initialCountdownTime,
+			hasUserAnswered: false
+		})
 	}
 }
 
