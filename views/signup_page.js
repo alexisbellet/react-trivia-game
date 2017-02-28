42e6dd95d5c50fa25d1a0a058d014210b6434b88
Alexis Bellet <info@alexis-bellet.com>
2017-02-28 18:26:11 -0500

    fix: add firebase deploy stuff

--- views/signup_page.js
+++ views/signup_page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import firebase from 'firebase';
-import {Link} from 'react-router';
+import { Link, browserHistory } from 'react-router';
 
 class SignUpPage extends React.Component {
 	constructor() {
